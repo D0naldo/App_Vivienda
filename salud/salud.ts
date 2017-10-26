ac1a84c6ae2d4c58fc3b028a604d3c4beee01f83
D0naldo <edwinretty@gmail.com>
2017-10-26 02:19:07 -0500

    actulizado

--- salud/salud.ts
+++ salud/salud.ts
@@ -3,14 +3,6 @@ import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { IonicPage, NavController } from 'ionic-angular';
 import { HomePage} from '../home/home';
 
-/**
- * Generated class for the SaludPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-salud',
   templateUrl: 'salud.html',
@@ -19,14 +11,12 @@ export class SaludPage {
   myForm: FormGroup;
   constructor(public navCtrl: NavController,
     public fb: FormBuilder) {
-    this.myForm = this.fb.group({
-      name: ['', [Validators.required]],
-      company: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
-      email: ['', [Validators.required, Validators.email]],
-      age: ['', [Validators.required]],
-      url: ['', [Validators.required, Validators.pattern(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)]],
-      password: ['', [Validators.required, Validators.pattern(/^[a-z0-9_-]{6,18}$/)]],
-    });
+      this.myForm = this.fb.group({
+        number1: ['', [Validators.required, Validators.pattern(/^[1-9]\d*$/)]],
+        number2: ['', [Validators.required, Validators.pattern(/^[1-9]\d*$/)]],
+        number3: ['', [Validators.required, Validators.pattern(/^[1-9]\d*$/)]],
+        number6: ['', [Validators.pattern(/^[1-9]\d*$/)]]
+          });
   }
   saveData(){
     alert(JSON.stringify(this.myForm.value));
